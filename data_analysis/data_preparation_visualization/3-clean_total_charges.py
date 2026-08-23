bcbe82af1de01285751527bac036faff1a63ee99
Anman-nn <endricos@gmail.com>
2026-08-23 22:24:06 +0200

    task Data Preparation

--- data_analysis/data_preparation_visualization/3-clean_total_charges.py
+++ data_analysis/data_preparation_visualization/3-clean_total_charges.py
@@ -4,9 +4,11 @@
 
 def clean_total_charges(df, method='drop'):
     '''conversion'''
+    import pandas as pd
     if method == 'drop':
         df.dropna(subset=["TotalCharges"], inplace=True)
     elif method == 'median':
+        df["TotalCharges"] = pd.to_numeric(df["TotalCharges"], errors="coerce")
         df['TotalCharges'] = df['TotalCharges'].fillna(df['TotalCharges'].median())
     elif method == 'impute':
         df['TotalCharges'] = df['TotalCharges'].fillna(df['MonthlyCharges']*df['tenure'])
