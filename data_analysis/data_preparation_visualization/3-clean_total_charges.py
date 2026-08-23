53f8bd09e110e2a8f23c8f953cd94d2c436349c7
Anman-nn <endricos@gmail.com>
2026-08-23 22:32:08 +0200

    task Data Preparation

--- data_analysis/data_preparation_visualization/3-clean_total_charges.py
+++ data_analysis/data_preparation_visualization/3-clean_total_charges.py
@@ -2,16 +2,24 @@
 '''Data Preparation and Visualization'''
 
 
-def clean_total_charges(df, method='drop'):
-    '''conversion'''
+def clean_total_charges(df, method="drop"):
+    """Clean TotalCharges column."""
     import pandas as pd
 
+    df = df.copy()
     df["TotalCharges"] = pd.to_numeric(df["TotalCharges"], errors="coerce")
-    if method == 'drop':
-        df.dropna(subset=["TotalCharges"], inplace=True)
-    elif method == 'median':
-        df['TotalCharges'] = df['TotalCharges'].fillna(df["TotalCharges"].median())
-    elif method == 'impute':
-        df['TotalCharges'] = df['TotalCharges'].fillna(df['MonthlyCharges']*df['tenure'])
+
+    if method == "drop":
+        df = df.dropna(subset=["TotalCharges"])
+
+    elif method == "median":
+        df["TotalCharges"] = df["TotalCharges"].fillna(
+            df["TotalCharges"].median()
+        )
+
+    elif method == "impute":
+        df["TotalCharges"] = df["TotalCharges"].fillna(
+            df["MonthlyCharges"] * df["tenure"]
+        )
+
     return df
- 
