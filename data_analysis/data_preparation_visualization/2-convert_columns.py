9261f4566fcee4b8e0abdaab7c714ed2eb59442b
Anman-nn <endricos@gmail.com>
2026-08-23 19:55:14 +0200

    task Data Preparation

--- data_analysis/data_preparation_visualization/2-convert_columns.py
+++ data_analysis/data_preparation_visualization/2-convert_columns.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 
 def convert_columns(df):
+    '''conversion'''
     df.TotalCharges = pd.to_numeric(df.TotalCharges, errors='coerce')
-    df["SeniorCitizen"] = df["SeniorCitizen"].map({"0": "No", "1": "Yes"})
+    df["SeniorCitizen"] = df["SeniorCitizen"].map({"0": "No", "1": "Yes"}).fillna("No")
     return df
