bfc952b9453309744dc96f6c88e210fdfbf1c7ec
Anman-nn <endricos@gmail.com>
2026-08-23 19:44:49 +0200

    task Data Preparation

---
+++ data_analysis/data_preparation_visualization/2-convert_columns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Data Preparation and Visualization'''
+
+import pandas as pd
+
+
+def convert_columns(df):
+    df.TotalCharges = pd.to_numeric(df.TotalCharges, errors='coerce')
+    df["SeniorCitizen"] = df["SeniorCitizen"].map({"0": "No", "1": "Yes"})
+    return df
