2b3d206c11543446cea8edfc2157c22d019e43c9
Anman-nn <endricos@gmail.com>
2026-08-23 22:06:55 +0200

    task Data Preparation

---
+++ data_analysis/data_preparation_visualization/3-clean_total_charges.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''Data Preparation and Visualization'''
+
+import pandas as pd
+
+
+def clean_total_charges(df, method='drop'):
+    '''conversion'''
+    if method == 'drop':
+        df.dropna(subset=["TotalCharges"], inplace=True)
+    elif method == 'median':
+        df['TotalCharges'] = df['TotalCharges'].fillna(df['TotalCharges'].median())
+    elif method == 'impute':
+        df['TotalCharges'] = df['TotalCharges'].fillna(df['MonthlyCharges']*df['tenure'])
+    return df
+ 
