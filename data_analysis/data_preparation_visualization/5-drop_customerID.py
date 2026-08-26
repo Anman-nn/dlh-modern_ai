0757c73e0924d7183a1bf2003f1771f4de0eab3e
Anman-nn <endricos@gmail.com>
2026-08-26 21:51:27 +0200

    task Data Preparation

---
+++ data_analysis/data_preparation_visualization/5-drop_customerID.py
@@ -0,0 +1,8 @@
+ #!/usr/bin/env python3
+'''Data Preparation and Visualization'''
+
+
+def drop_customerID(df):
+    '''def drop_customerID(df):'''
+    df = df.drop('customerID', axis=1)
+    return df
