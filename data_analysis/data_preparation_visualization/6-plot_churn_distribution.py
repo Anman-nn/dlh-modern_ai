1318cde89d2ce3c45805836962af33a9e86a6bbc
Anman-nn <endricos@gmail.com>
2026-08-26 22:08:55 +0200

    task Data Preparation

---
+++ data_analysis/data_preparation_visualization/6-plot_churn_distribution.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''Data Preparation and Visualization'''
+
+
+import matplotlib.pyplot as plt
+
+
+def plot_churn_distribution(df):
+    """
+    """
+    plt.figure(figsize=(12, 8))
+    plt.title('Churn Distribution')
+    plt.ylabel('Count')
+    plt.bar(["No", "Yes"], df.Churn.value_counts(), color=["skyblue", "salmon"])
+    plt.show()
