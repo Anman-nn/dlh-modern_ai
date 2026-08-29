2310c440ba0e34ea0c5dbd32428aee26f88c771d
Anman-nn <endricos@gmail.com>
2026-08-29 15:33:05 +0200

    task Data Preparation

--- data_analysis/data_preparation_visualization/9-plot_correlation_heatmap.py
+++ data_analysis/data_preparation_visualization/9-plot_correlation_heatmap.py
@@ -15,10 +15,14 @@ def plot_correlation_heatmap(df):
     sns.heatmap(
         corr,
         annot=True,
+        fmt=".2g",
         cmap="coolwarm",
         vmin=-1,
-        vmax=1
+        vmax=1,
+        center=0,
+        square=True
     )
+
     plt.title('Correlation Matrix')
     plt.tight_layout()
     plt.show()
