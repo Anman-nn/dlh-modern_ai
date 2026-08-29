e8606895b26df4072bd6d07da2132229adaf0ed3
Anman-nn <endricos@gmail.com>
2026-08-29 11:38:46 +0200

    task Data Preparation

---
+++ data_analysis/data_preparation_visualization/7-plot_categorical_distributions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+"""
+import matplotlib.pyplot as plt
+
+
+def plot_categorical_distributions(df, columns_to_plot=None):
+    """
+    """
+    import math
+    if columns_to_plot is None:
+        columns_to_plot = df.select_dtypes(include="object").columns.tolist()
+        columns_to_plot.remove('Churn')
+    else:
+        to_plot = columns_to_plot
+    n_cols, n_rows = 3, n_rows = math.ceil(len(columns_to_plot) / n_cols)
+    fig, axes = plt.subplots(n_rows, n_cols, figsize=(15, 5*n_rows))
+    axes = axes.flatten()
+
+    for i, col in enumerate(columns_to_plot):
+        counts = df[col].value_counts()
+        axes[i].bar(counts.index, counts.values)
+        axes[i].set_title(col)
+        axes[i].tick_params(axis="x", rotation=45)
+
+    for j in range(i + 1, len(axes)):
+        axes[j].axis("off")
+
+    plt.tight_layout()
+    plt.savefig("Task_7.png")
+    plt.show()
