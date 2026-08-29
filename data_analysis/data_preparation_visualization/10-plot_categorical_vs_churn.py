72196428508f271fe832d075669160553fce28dd
Anman-nn <endricos@gmail.com>
2026-08-29 16:17:21 +0200

    task Data Preparation

--- data_analysis/data_preparation_visualization/10-plot_categorical_vs_churn.py
+++ data_analysis/data_preparation_visualization/10-plot_categorical_vs_churn.py
@@ -7,7 +7,8 @@ import matplotlib.pyplot as plt
 
 def plot_categorical_vs_churn(df, col):
     '''plot_categorical_vs_churn'''
-    counts = df.groupby(col)['Churn'].value_counts(normalize=True).reset_index().query("Churn == 'Yes'")
+    counts = df.groupby(col)['Churn'].value_counts(normalize=True)\
+        .reset_index().query("Churn == 'Yes'")
     plt.figure(figsize=(12, 8))
     plt.bar(counts[col], counts['proportion'])
     plt.title(f'Churn Rate by {col}')
