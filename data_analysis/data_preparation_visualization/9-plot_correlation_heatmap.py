4cffe5612d5520155e6e24ef00a0d57894e6fe14
Anman-nn <endricos@gmail.com>
2026-08-29 15:24:58 +0200

    task Data Preparation

---
+++ data_analysis/data_preparation_visualization/9-plot_correlation_heatmap.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""plot_continuous_distributions
+"""
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def plot_correlation_heatmap(df):
+    """plot_correlation_heatmap
+    """
+    plt.figure(figsize=(6, 5))
+
+    corr = df.select_dtypes(include="number").corr()
+
+    sns.heatmap(
+        corr,
+        annot=True,
+        cmap="coolwarm",
+        vmin=-1,
+        vmax=1
+    )
+
+    plt.tight_layout()
+    plt.show()
+
+    return None
