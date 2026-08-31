a6bddfb31c0775aef0421957e24445db404aafdd
Anman-nn <endricos@gmail.com>
2026-08-31 21:38:45 +0200

    task Data Preparation

---
+++ data_analysis/data_preparation_visualization/12-chi_square_tests.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""plot_continuous_distributions
+"""
+
+import pandas as pd
+from scipy import stats
+
+
+def chi_square_tests(df):
+    '''chi square'''
+    f = df.select_dtypes(include="object").columns.tolist()
+    f.remove('Churn')
+    res = {}
+    for col in f:
+        table = pd.crosstab(df[col], df["Churn"])
+        chi2, p, dof, expected = stats.chi2_contingency(table)
+        res[col] = float(p)
+    return res
