874413e97f9da99ec265b995bc1f18ad5c748a5e
Anman-nn <endricos@gmail.com>
2026-09-01 07:08:55 +0200

    task Data Preparation

---
+++ data_analysis/data_preparation_visualization/17-split_data.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""plot_continuous_distributions
+"""
+
+from sklearn import model_selection
+
+
+def split_data(df, target='Churn', test_size=0.2, random_state=42):
+    '''model_selection'''
+
+    X = df.drop(columns=[target])
+    y = df[target]
+
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(
+        X,
+        y,
+        test_size=test_size,
+        random_state=random_state,
+        stratify=y
+    )
+
+    return X_train, X_test, y_train, y_test
