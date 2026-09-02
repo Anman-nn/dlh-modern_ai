b761668ccad1e0873141cbad9cc7158420a0c385
Anman-nn <endricos@gmail.com>
2026-09-02 07:05:07 +0200

    task Data Preparation

--- data_analysis/web_scraping/0-fetch_html.py
+++ data_analysis/web_scraping/0-fetch_html.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 """Data Collection - Web Scraping
 """
+
+import requests
+
+def fetch_html(url, headers = None, timeout = 10):
+
+
+    response = requests.get(url, headers=headers, timeout=timeout)
+    response.raise_for_status()
+    return response.text
