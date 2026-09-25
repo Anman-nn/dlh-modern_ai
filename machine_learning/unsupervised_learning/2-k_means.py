69228aa1e6e6207c5f85a15cf0fac34157ebf67d
Anman-nn <endricos@gmail.com>
2026-09-25 06:46:12 +0200

    task Data Preparation

--- machine_learning/unsupervised_learning/2-k_means.py
+++ machine_learning/unsupervised_learning/2-k_means.py
@@ -8,6 +8,10 @@ from sklearn import cluster
 def K_Means(X, n_clusters, random_state):
     '''K_Means(X, n_clusters, random_state)'''
 
-    kmeans = cluster.KMeans(n_clusters=n_clusters, random_state=random_state, n_init='auto')
+    kmeans = cluster.KMeans(
+        n_clusters=n_clusters,
+        random_state=random_state,
+        n_init='auto'
+    )
     kmeans.fit(X)
     return kmeans
