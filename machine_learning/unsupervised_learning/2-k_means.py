b6308b2594efadc4d91c8c582396ef884af5af55
Anman-nn <endricos@gmail.com>
2026-09-25 06:44:40 +0200

    task Data Preparation

---
+++ machine_learning/unsupervised_learning/2-k_means.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Unsupervised Learning
+"""
+
+from sklearn import cluster
+
+
+def K_Means(X, n_clusters, random_state):
+    '''K_Means(X, n_clusters, random_state)'''
+
+    kmeans = cluster.KMeans(n_clusters=n_clusters, random_state=random_state, n_init='auto')
+    kmeans.fit(X)
+    return kmeans
