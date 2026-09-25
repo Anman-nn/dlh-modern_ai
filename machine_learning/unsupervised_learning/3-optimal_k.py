442380b9a8e6d5dd80850699c35e548c845726e3
Anman-nn <endricos@gmail.com>
2026-09-25 07:20:42 +0200

    task Data Preparation

--- machine_learning/unsupervised_learning/3-optimal_k.py
+++ machine_learning/unsupervised_learning/3-optimal_k.py
@@ -8,5 +8,24 @@ K_Means = __import__('2-k_means').K_Means
 
 def optimal_k(X, max_clusters, random_state):
     '''optimal_k(X, max_clusters, random_state)'''
+    K = range(2, max_clusters+1)
+    fits = []
+    inertia_values = []
+    silhouette_values = []
+    ks = []
 
-    return list(range(2,10))
+    for k in K:
+        # train the model for current value of k on training data
+        model = K_Means(X, k, random_state)
+        
+        # append the model to fits
+        fits.append(model)
+        
+        inertia_values.append(model.inertia_)
+
+        # Append the silhouette score to scores
+        silhouette_values.append(metrics.silhouette_score(X, model.labels_, metric='euclidean'))
+
+        ks.append(k)
+
+    return ks, inertia_values, silhouette_values
