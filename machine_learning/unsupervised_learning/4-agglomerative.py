997b173a77db5cca8def0963f7678c93d16ce0de
Anman-nn <endricos@gmail.com>
2026-09-26 18:34:12 +0200

    task Data Preparation

--- machine_learning/unsupervised_learning/4-agglomerative.py
+++ machine_learning/unsupervised_learning/4-agglomerative.py
@@ -7,9 +7,25 @@ from sklearn import metrics
 Apply_PCA = __import__('1-pca').Apply_PCA
 
 
-def Agglomerative_Clustering(X, n_clusters, random_state, n_components, use_pca_data=True):
+def Agglomerative_Clustering(
+    X,
+    n_clusters,
+    random_state,
+    n_components,
+    use_pca_data=True
+):
     if use_pca_data:
-        X = Apply_PCA(X, n_components, random_state)
-    model = cluster.AgglomerativeClustering(n_clusters = n_clusters)
-    y_means = model.fit_predict(X)
-    return y_means, X, metrics.silhouette_score(X, y_means)
+        X, pca_model = Apply_PCA(
+            X,
+            n_components,
+            random_state
+        )
+
+    model = cluster.AgglomerativeClustering(
+        n_clusters=n_clusters
+    )
+
+    labels = model.fit_predict(X)
+    score = metrics.silhouette_score(X, labels)
+
+    return model, X, score
